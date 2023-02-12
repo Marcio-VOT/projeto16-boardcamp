04d32ba486dbf8523fb3e77af1ffd1233f84ba2c
Marcio-VOT <marciofsantos65@gmail.com>
2023-02-12 17:59:30 -0300

    fix: definindo um pardrão de caracteres para o cpf e o phone do cliente

--- src/schemas/clientsInfoSchema.js
+++ src/schemas/clientsInfoSchema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const schema = Joi.object({
     name : Joi.string().required(),
-    phone : Joi.string().min(10).max(11).required(),
-    cpf : Joi.string().length(11).required(),
+    phone : Joi.string().min(10).max(11).pattern(/^[0-9]+$/).required(),
+    cpf : Joi.string().length(11).pattern(/^[0-9]+$/).required(),
     birthday : Joi.date().required()
 })
