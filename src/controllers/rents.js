95db55e059da4a65451458929fad9c804c02bbe4
Marcio-VOT <marciofsantos65@gmail.com>
2023-02-13 15:20:13 -0300

    feat: registro de rental parcial e listar rentals completo

--- src/controllers/rents.js
+++ src/controllers/rents.js
@@ -1,8 +1,19 @@
 import { db } from "../config/database.js";
+import dayjs from "dayjs";
 
 export async function rentsList(req, res) {
     try {
-        
+        const { rows } = await db.query(`
+        SELECT rentals.*,
+        JSON_BUILD_OBJECT('id', customers.id, 'name', customers.name) AS customer,
+        JSON_BUILD_OBJECT('id', games.id, 'name', games.name) AS game
+        FROM rentals 
+        JOIN customers 
+        ON rentals."customerId"=customers.id
+        JOIN games
+        ON rentals."gameId" = games.id;
+        `)
+        res.send(rows);
     } catch (error) {
         res.status(500).send(error.message)
     }
@@ -10,7 +21,12 @@ export async function rentsList(req, res) {
 
 export async function rentInsert(req, res) {
     try {
-        
+        const { customerId, gameId, daysRented} = req.body;
+        const { rows } = await db.query('SELECT * FROM games WHERE id=$1;', [gameId]);
+        const originalPrice = rows[0].pricePerDay*daysRented;
+        console.log(req.body, rows, originalPrice)
+        await db.query('INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES($1, $2, $3, $4, $5, $6, $7);',[customerId, gameId, dayjs().format('YYYY-MM-DD'), daysRented, null, originalPrice, null] );
+        res.sendStatus(200)
     } catch (error) {
         res.status(500).send(error.message)
     }
